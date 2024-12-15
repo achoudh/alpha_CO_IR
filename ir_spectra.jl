e4d5b0785ff169b3d7db7cbf18f8508b25bfc092
DannyCrowley <58996373+DannyCrowley@users.noreply.github.com>
2024-12-15 15:19:56 +0100

    Testing convergence with periodic boundary in z

--- ir_spectra.jl
+++ ir_spectra.jl
@@ -16,7 +16,7 @@ function hstatT(ev::Vector{Float64}, eu::Vector{Vector{Float64}}, com_ol::Matrix
             rvec12::Vector{Float64} = com_ol[n1,:] - com_ol[n2,:]
             rvec12[1] = rvec12[1] - nx*round(Int, rvec12[1]/(nx)) # periodic boundary conditions in x
             rvec12[2] = rvec12[2] - ny*round(Int, rvec12[2]/(ny)) # periodic boundary conditions in y
-            #rvec12[3] = rvec12[3] - ny*round(Int, rvec12[3]/(nz)) # no periodic boundary conditions in z
+            rvec12[3] = rvec12[3] - ny*round(Int, rvec12[3]/(nz)) # no periodic boundary conditions in z
 
             rvec12 = a0_CO .* rvec12
             r12::Float64 = norm(rvec12)
